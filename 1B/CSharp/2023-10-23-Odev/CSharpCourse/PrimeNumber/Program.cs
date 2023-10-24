b268d2a895dc3dd9776d96c2089d7149c9e39227
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-24 16:16:07 +0300

    Prime Number

---
+++ 1B/CSharp/2023-10-23-Odev/CSharpCourse/PrimeNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            result = false;
+            //break;
+            i = number;
+        }
+    }
+
+    return result;
+}
+
+Console.WriteLine(IsPrimeNumber(89));

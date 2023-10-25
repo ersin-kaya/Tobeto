cfbf86729b04d6a1743d40831c45c050846fec5c
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-10-25 22:47:21 +0300

    RecapDemo1

---
+++ 1B/CSharp/Assignment-Day2/CSharpCourse/RecapDemo1/Program.cs
@@ -0,0 +1,28 @@
+﻿GenerateButtons();
+
+//macOS
+static void GenerateButtons()
+{
+    string[,] buttons = new string[8, 8];
+
+    for (int i = 0; i <= buttons.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j <= buttons.GetUpperBound(1); j++)
+        {
+            buttons[i, j] = j.ToString();
+
+            if ((i + j) % 2 == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.Write("| {0} |", buttons[i, j]);
+        }
+        Console.WriteLine();
+    }
+}

ec73d7241e222fe05439ccf34ffa1663926a2b1c
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-11-03 12:45:36 +0300

    Get the first 10 perfect numbers - refactor

--- 1B/CSharp/Lesson-Day3/Projects/PerfectNumbers/Program.cs
+++ 1B/CSharp/Lesson-Day3/Projects/PerfectNumbers/Program.cs
@@ -1,5 +1,7 @@
-﻿//7 - Write a C# program finds first 10 perfect numbers.
+﻿using System.Diagnostics;
 using System.Numerics;
+
+//7 - Write a C# program finds first 10 perfect numbers.
 
 var firstTenPerfectNumbers = GetFirstTenPerfectNumbers(GetPrimeNumbersUpTo(100));
 
@@ -11,18 +13,19 @@ foreach (BigInteger perfectNumber in firstTenPerfectNumbers)
 static List<BigInteger> GetFirstTenPerfectNumbers(List<int> primeNumbers)
 {
     List<BigInteger> perfectNumbers = new List<BigInteger>();
-    short perfectNumbersCount = (short)perfectNumbers.Count();
+    int perfectNumbersCount = perfectNumbers.Count();
 
-    foreach (int primeNumber in primeNumbers)
+    foreach (BigInteger primeNumber in primeNumbers)
     {
         //n asal iken, (2^n)-1'de asal ise 2^(n-1)*((2^n)-1) => perfect number
-        if (!IsDividerMoreThanTwo(ComputeThePowerOfTwo(primeNumber) - 1))
+        if (IsPrime(ComputeThePowerOfTwo(primeNumber) - 1))
         {
             BigInteger perfectNumber = ComputeThePowerOfTwo(primeNumber - 1) * (ComputeThePowerOfTwo(primeNumber) - 1);
+
             perfectNumbers.Add(perfectNumber);
             Console.WriteLine(perfectNumber);
 
-            if (++perfectNumbersCount == 10) //örn. ilk 8 sayıyı almak isterseniz bir süre beklettikten sonra sonuç dönecektir, veya doğrudan ekrana yazdırılırsa sadece 8. sayı geç yazdırılacaktır...
+            if (++perfectNumbersCount == 10)
             {
                 break;
             }
@@ -37,7 +40,7 @@ static List<int> GetPrimeNumbersUpTo(int number)
 
     for (int i = 1; i < number; i++)
     {
-        if (!IsDividerMoreThanTwo(i))
+        if (IsPrime(i))
         {
             primeNumbers.Add(i);
         }
@@ -45,25 +48,128 @@ static List<int> GetPrimeNumbersUpTo(int number)
     return primeNumbers;
 }
 
-//son 2 sayının üretilmesine engel olan, refactor edilmesi gereken metot
-static bool IsDividerMoreThanTwo(BigInteger number)
+//Fermat asallık testi uygulanmaya çalışılmıştır...
+static bool FermatPrimalityTest(BigInteger number)
 {
-    int counter = 0;
-    for (BigInteger i = 1; i <= number; i++) //not: döngüyü number/2'ye gidecek şekilde ele almak çözüm olmuyor
+    //1. number asallığını kontrol edeceğimiz tam sayı olmak üzere
+
+    Random random = new Random();
+    BigInteger randomNumber = NextBigInteger(random, 1, number); //2. number'dan küçük rastgele bir tam sayı üretiyoruz
+
+    //3. number ve randomNumber'ın en büyük ortak böleni 1 midir?
+    if (GCD(number, randomNumber) == 1)
     {
-        if (number % i == 0)
-        {
-            counter++;
-        }
-        if (counter > 2)
+        //5. en büyük ortak bölenleri 1 ise randomNumber^(number-1) % number'ın 1'e eşit olup olmadığına bakılır
+        if (ComputeThePowerOf(randomNumber, number - 1) % number == 1)
         {
             return true;
         }
+        else
+        {
+            //6. cevap hayır ise number'ın bileşik sayı olduğuna dair bir kanıt elde etmiş oluyoruz
+            return false;
+        }
     }
-    return (counter == 1) ? true : false;
+    else
+    { //4. en büyük ortak bölen 1'den büyük ise bunların ortak bir çarpanı vardır ve bu durumda number bileşik sayıdır
+        return false;
+    }
+}
+
+static bool IsPrime(BigInteger number)
+{
+    List<bool> bools = new List<bool>();
+    int numberTrials = 0;
+
+    while (++numberTrials != 5)
+    {
+        bools.Add(FermatPrimalityTest(number));
+    }
+
+    if (bools.Contains(false))
+    {
+        return false;
+    }
+    return true;
 }
 
 static BigInteger ComputeThePowerOfTwo(BigInteger power)
 {
-    return (BigInteger)Math.Pow(2, (double)power); //refactor
+    BigInteger counter = 1;
+    BigInteger result = 2;
+
+    while (counter++ < power)
+    {
+        result *= 2;
+    }
+    return result;
 }
+
+static BigInteger ComputeThePowerOf(BigInteger number, BigInteger power)
+{
+    BigInteger counter = 1;
+    BigInteger result = number;
+
+    while (counter++ < power)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+static BigInteger GCD(BigInteger a, BigInteger b)
+{
+    while (a != 0 && b != 0)
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+    return a | b;
+}
+
+static BigInteger NextBigInteger(Random random, BigInteger minValue, BigInteger maxValue)
+{
+    if (minValue > maxValue) throw new ArgumentException();
+    if (minValue == maxValue) return minValue;
+    BigInteger zeroBasedUpperBound = maxValue - 1 - minValue; // Inclusive
+    Debug.Assert(zeroBasedUpperBound.Sign >= 0);
+    byte[] bytes = zeroBasedUpperBound.ToByteArray();
+    Debug.Assert(bytes.Length > 0);
+    Debug.Assert((bytes[bytes.Length - 1] & 0b10000000) == 0);
+
+    // Search for the most significant non-zero bit
+    byte lastByteMask = 0b11111111;
+    for (byte mask = 0b10000000; mask > 0; mask >>= 1, lastByteMask >>= 1)
+    {
+        if ((bytes[bytes.Length - 1] & mask) == mask) break; // We found it
+    }
+
+    while (true)
+    {
+        random.NextBytes(bytes);
+        bytes[bytes.Length - 1] &= lastByteMask;
+        var result = new BigInteger(bytes);
+        Debug.Assert(result.Sign >= 0);
+        if (result <= zeroBasedUpperBound) return result + minValue;
+    }
+}
+
+////son 2 sayının üretilmesine engel olan, refactor edilmesi gereken metot
+//static bool IsDividerMoreThanTwo(Int128 number)
+//{
+//    int counter = 0;
+//    for (Int128 i = 1; i <= number; i++) //not: döngüyü number/2'ye gidecek şekilde ele almak çözüm olmuyor
+//    {
+//        if (number % i == 0)
+//        {
+//            counter++;
+//        }
+//        if (counter > 2)
+//        {
+//            return true;
+//        }
+//    }
+//    return (counter == 1) ? true : false;
+//}

192b5cf35690ab6fcf00864b34b0649d22e57b36
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-11-19 23:53:29 +0300

    CSharp, Assignment-Day10_12

---
+++ 1B/CSharp/Assignment-Day10_12/TobetoCourseAcademy/DataAccess/Concretes/EntityFramework/TobetoCourseAcademyContext.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+    public class TobetoCourseAcademyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string serverAddress = IConnectionString.ServerAddress;
+            string database = IConnectionString.Database;
+            string username = IConnectionString.Username;
+            string password = IConnectionString.Password;
+            string connectionString = string.Format("Server={0};Database={1};User={2};Password={3};Encrypt=False;", serverAddress, database, username, password);
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            //optionsBuilder.UseSqlServer("Server=localhost;Database=TobetoCourseAcademy;Trusted_Connection= true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CourseInstructor> CourseInstructors { get; set; }
+    }
+}

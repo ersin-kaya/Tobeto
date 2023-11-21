dcb54cb14772fabbb24bd5574c7e11043f4869b8
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2023-11-21 22:39:03 +0300

    CSharp, Assignment-Day13

---
+++ 1B/CSharp/Assignment-Day13/FinalProject/DataAccess/Abstracts/IConnectionString.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccess.Abstracts
+{
+    interface IConnectionString
+    {
+        internal static string ServerAddress = "localhost,1433";
+        internal static string Database = "Northwind";
+        internal static string Username = "SA";
+        internal static string Password = "y0sTP4sswd";
+    }
+}
+
---
+++ 1B/CSharp/Assignment-Day13/FinalProject/DataAccess/Concretes/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+    public class NorthwindContext : DbContext, IConnectionString
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string serverAddress = IConnectionString.ServerAddress;
+            string database = IConnectionString.Database;
+            string username = IConnectionString.Username;
+            string password = IConnectionString.Password;
+            string connectionString = string.Format("Server={0};Database={1};User={2};Password={3};Encrypt=False;", serverAddress, database, username, password);
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
+
